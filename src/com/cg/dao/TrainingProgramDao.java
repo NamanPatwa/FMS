8039cf27474cc8f0d864ba4197d237e47dbaebf3
Jyoti <jyoti130896@gmail.com>
2019-10-19 04:39:20 -0700

    "Pushed TrainingProgramDaoImpl"

--- src/com/cg/dao/TrainingProgramDao.java
+++ src/com/cg/dao/TrainingProgramDao.java
@@ -1,5 +1,24 @@
 package com.cg.dao;
 
-public interface TrainingProgramDao {
+import java.util.List;
 
+import com.cg.bean.TrainingProgram;
+import com.cg.exception.TrainingProgramNotFoundException;
+
+public interface TrainingProgramDao {
+	String fetchAllTrainingProgramQuery = "SELECT * from TRAINING_MASTER ";
+	String fetchTrainingProgramByTrainingCodeQuery = "SELECT * from TRAINING_MASTER " + "where= TRAINING_CODE=?";
+	String updateTrainingProgramQuery = "UPDATE TRAINING_MASTER SET FACULTY_CODE=?, "
+			+ "COURSE_CODE=?, START_DATE = ?, END_DATE = where TRAINING_CODE=?";
+	String removeTrainingProgramQuery = "DELETE from TRAINING_MASTER where TRAINING_CODE=?";
+	
+    List<TrainingProgram> fetchAllTrainingProgram() throws TrainingProgramNotFoundException;
+	
+	TrainingProgram fetchTrainingProgramByTrainingCode(int code) throws TrainingProgramNotFoundException;
+	
+	TrainingProgram updateTrainingProgram(int code) throws TrainingProgramNotFoundException;
+	
+	boolean removeTrainingProgram(int code) throws TrainingProgramNotFoundException;
+	
+	
 }
