a9e826e946315181e4823c19fab32d4f828dc7fe
lenovo <lenovo@LAPTOP-2NI1DNQF>
2019-10-20 23:52:56 +0530

    Faculty Dao incomplete

---
+++ src/com/cg/client/FacultyCli.java
@@ -0,0 +1,69 @@
+package com.cg.client;
+
+import java.util.Scanner;
+
+import com.cg.bean.Faculty;
+import com.cg.exception.FacultyDoesNotExist;
+import com.cg.service.FacultyService;
+import com.cg.service.FacultyServiceImpl;
+
+public class FacultyCli {
+
+	private static Scanner console;
+	static {
+		console = new Scanner(System.in);
+	}
+
+	public static void main(String[] args) {
+		int option = 0;
+		
+		while(true) {
+			System.out.println("1-Add Faculty details.");
+			System.out.println("2-Update Faculty details by faculty id.");
+			System.out.println("3-System Exit.");
+			option = console.nextInt();
+			
+			switch(option) {
+			case 1: addFacultyDetails(); break;
+			case 2: updateFacultyDetails(); break;
+			case 3: System.exit(0);
+			default: System.out.println("Please add valid information..");
+			}
+		}
+	}
+
+	private static void updateFacultyDetails() {
+		
+	}
+
+	private static void addFacultyDetails() {
+		FacultyService service = new FacultyServiceImpl();
+		String facultyId, skillset;
+		
+		do {
+			System.out.println("Enter Faculty Id: ");
+			facultyId = console.next();
+		} while(!service.validateFacultyId(facultyId));
+			
+		do {
+			System.out.println("Enter Faculty skillset: ");
+			skillset = console.next();
+		} while(!service.validateFacultySkillset(skillset));
+					
+		Faculty faculty = new Faculty();
+//		String previousSkillset = faculty.getSkillSet();
+//		String newSkillset = previousSkillset.concat(skillset);
+		
+		faculty.setFacultyId(Integer.parseInt(facultyId));
+		faculty.setSkillSet(skillset);
+		
+		try {
+			int id = service.saveFacultySkillset(faculty);
+			System.out.println("Faculty details are saved with id: " + id);
+		} catch (FacultyDoesNotExist e) {
+			System.out.println("Faculty details are not saved..");
+		}
+		
+		
+	}
+}
